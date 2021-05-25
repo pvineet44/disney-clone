74259afeb7d2c33a8d166ade2ac5b47f29f3a590
Vineet Parekh <pvineet44@gmail.com>
2021-05-25 13:53:44 +0530

    Login OAuth gmail

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,155 +1,191 @@
 import styled from 'styled-components';
 import { auth, provider } from '../Firebase';
-// import Login from './Login';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import {
+  selectUserName,
+  selectUserPhoto,
+  selectUserEmail,
+  setUserLoginDetails,
+} from '../features/user/userSlice';
 
 const Header = (props) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const username = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
 
-    const handleAuth = () => {
-        auth.signInWithPopup(provider).then((result) => {
-            console.log(result)
-        }).catch((error) => {
-            alert(error.message);
-        });
-    }
+  const handleAuth = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        setUser(result.user);
+        console.log(result);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
 
-    return (
-        <Nav>
-            <Logo>
-            <img src='/images/logo.svg' alt=''/>
-            </Logo>
-            <NavMenu>
-                <a href='/home'>
-                    <img src='/images/home-icon.svg' alt='HOME'/>
-                <span>HOME</span>
-                </a>
-                <a href='/search'>
-                    <img src='/images/search-icon.svg' alt='SEARCH'/>
-                <span>SEARCH</span>
-                </a>
-                <a href='/watchlist'>
-                    <img src='/images/watchlist-icon.svg' alt='WATCHLIST'/>
-                <span>WATCHLIST</span>
-                </a>
-                <a href='/original'>
-                    <img src='/images/original-icon.svg' alt='ORIGINAL'/>
-                <span>ORIGINAL</span>
-                </a>
-                <a href='/series'>
-                    <img src='/images/series-icon.svg' alt='SERIES'/>
-                <span>SERIES</span>
-                </a>
-            </NavMenu>
-            <Login onClick={handleAuth}>Login</Login>
-        </Nav>
-    )
-}
+  const setUser = (user) => {
+    dispatch(
+      setUserLoginDetails({
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+      })
+    );
+  };
+
+  return (
+    <Nav>
+      <Logo>
+        <img src='/images/logo.svg' alt='' />
+      </Logo>
+
+      {!username ? (
+        <Login onClick={handleAuth}>Login</Login>
+      ) : (
+        <>
+          <NavMenu>
+            <a href='/home'>
+              <img src='/images/home-icon.svg' alt='HOME' />
+              <span>HOME</span>
+            </a>
+            <a href='/search'>
+              <img src='/images/search-icon.svg' alt='SEARCH' />
+              <span>SEARCH</span>
+            </a>
+            <a href='/watchlist'>
+              <img src='/images/watchlist-icon.svg' alt='WATCHLIST' />
+              <span>WATCHLIST</span>
+            </a>
+            <a href='/original'>
+              <img src='/images/original-icon.svg' alt='ORIGINAL' />
+              <span>ORIGINAL</span>
+            </a>
+            <a href='/series'>
+              <img src='/images/series-icon.svg' alt='SERIES' />
+              <span>SERIES</span>
+            </a>
+          </NavMenu>
+          <UserImg src={userPhoto} alt={username} />
+        </>
+      )}
+      {/* <Login onClick={handleAuth}>Login</Login> */}
+    </Nav>
+  );
+};
 
 const Logo = styled.a`
-    padding: 0;
-    width: 80px;
-    margin-top: 4px;
-    max-height: 70px;
-    font-size: 0;
-    display: inline-block;
-    img {
-        display: block;
-        width: 100%;
-    }
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+  img {
+    display: block;
+    width: 100%;
+  }
 `;
 
 const Nav = styled.nav`
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    height: 70px;
-    background-color: #090b13;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 36px;
-    letter-spacing: 16px;
-    z-index: 3;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
 `;
 
 const NavMenu = styled.div`
-    align-items: center;
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+
+  a {
     display: flex;
-    flex-flow: row nowrap;
-    height: 100%;
-    justify-content: flex-end;
-    position: relative;
-    margin-right: auto;
-    margin-left: 25px;
+    align-items: center;
+    padding: 0 12px;
 
-    a {
-        display: flex;
-        align-items: center;
-        padding: 0 12px;
-
-        img {
-            height: 20px;
-            min-width: 20px;
-            width: 20px;
-            z-index: auto;
-        }
-
-        span {
-            color: rgb(249, 249, 249);
-            font-size: 13px;
-            letter-spacing: 1.42px;
-            line-height: 1.08;
-            padding: 2px 0px;
-            white-space: nowrap;
-            position: relative;
-            margin-left: 10px;
-
-            &:before {
-                background-color: rgb(249, 249, 249);
-                border-radius: 0px 0px 4px 4px;
-                bottom: -6px;
-                content: '';
-                left: 0px;
-                height: 2px;
-                opacity: 0;
-                position: absolute;
-                right: 0px;
-                transform-origin: left center;
-                transform: scaleX(0);
-                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-                visibility: hidden;
-                width: auto;
-            }
-        }
-
-
-            &:hover {
-                span: before {
-                    transform: scaleX(1);
-                    visibility: visible;
-                    opacity: 1 !important;
-            }
-        }
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
     }
-    @media (max-width: 768px) {
-        display: none;
+
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
+      margin-left: 10px;
+
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: '';
+        left: 0px;
+        height: 2px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
     }
+
+    &:hover {
+      span: before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const Login = styled.a`
-    background-color: rgba(0, 0, 0, 0.6);
-    padding: 8px 16px;
-    text-transform: uppercase;
-    letter-spacing: 1.5px;
-    border: 1px solid #f9f9f9;
-    border-radius: 4px;
-    transition: 200ms ease-out;
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: 200ms ease-out;
 
-    &:hover {
-        background-color: #f9f9f9;
-        color: #000;
-        border-color: transparent;
-    }
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
+
+const UserImg = styled.img`
+    height: 100%;
 `;
 
 export default Header;
