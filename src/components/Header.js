5b5c5e9ee60bc019280b73fb0996b58e15504936
Vineet Parekh <pvineet44@gmail.com>
2021-05-25 15:52:35 +0530

    adding slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,9 @@ import {
   selectUserPhoto,
   selectUserEmail,
   setUserLoginDetails,
+  setSignOutState,
 } from '../features/user/userSlice';
+import { useEffect } from 'react';
 
 const Header = (props) => {
   const dispatch = useDispatch();
@@ -15,16 +17,34 @@ const Header = (props) => {
   const username = useSelector(selectUserName);
   const userPhoto = useSelector(selectUserPhoto);
 
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUser(user);
+        history.push('/home');
+      }
+    });
+  }, [username]);
+
   const handleAuth = () => {
-    auth
-      .signInWithPopup(provider)
-      .then((result) => {
-        setUser(result.user);
-        console.log(result);
-      })
-      .catch((error) => {
+    if (!username) {
+      auth
+        .signInWithPopup(provider)
+        .then((result) => {
+          setUser(result.user);
+          console.log(result);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else if (username) {
+      auth.signOut().then(() => {
+        dispatch(setSignOutState());
+        history.push('/');
+      }).catch((error) => {
         alert(error.message);
       });
+    }
   };
 
   const setUser = (user) => {
@@ -69,7 +89,12 @@ const Header = (props) => {
               <span>SERIES</span>
             </a>
           </NavMenu>
-          <UserImg src={userPhoto} alt={username} />
+          <SignOut>
+            <UserImg src={userPhoto} alt={username} />
+            <DropDown>
+              <span onClick={handleAuth}>Sign out</span>
+            </DropDown>
+          </SignOut>
         </>
       )}
       {/* <Login onClick={handleAuth}>Login</Login> */}
@@ -185,7 +210,45 @@ const Login = styled.a`
 `;
 
 const UserImg = styled.img`
+  height: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 40px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  ${UserImg} {
+    border-radius: 50%;
+    /* width: 100%; */
     height: 100%;
+  }
+
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
 `;
 
 export default Header;
