a0afbb0e126c1d64abe5be51f8100c8970d3c1b1
Vineet Parekh <pvineet44@gmail.com>
2021-05-25 11:37:43 +0530

    Homepage fin

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,28 @@ const Header = (props) => {
             <Logo>
             <img src='/images/logo.svg' alt=''/>
             </Logo>
+            <NavMenu>
+                <a href='/home'>
+                    <img src='/images/home-icon.svg' alt='HOME'/>
+                <span>HOME</span>
+                </a>
+                <a href='/search'>
+                    <img src='/images/search-icon.svg' alt='SEARCH'/>
+                <span>SEARCH</span>
+                </a>
+                <a href='/watchlist'>
+                    <img src='/images/watchlist-icon.svg' alt='WATCHLIST'/>
+                <span>WATCHLIST</span>
+                </a>
+                <a href='/original'>
+                    <img src='/images/original-icon.svg' alt='ORIGINAL'/>
+                <span>ORIGINAL</span>
+                </a>
+                <a href='/series'>
+                    <img src='/images/series-icon.svg' alt='SERIES'/>
+                <span>SERIES</span>
+                </a>
+            </NavMenu>
         </Nav>
     )
 }
@@ -36,6 +58,70 @@ const Nav = styled.nav`
     padding: 0 36px;
     letter-spacing: 16px;
     z-index: 3;
+`;
+
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    height: 100%;
+    justify-content: flex-end;
+    position: relative;
+    margin-right: auto;
+    margin-left: 25px;
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+            height: 20px;
+            min-width: 20px;
+            width: 20px;
+            z-index: auto;
+        }
+
+        span {
+            color: rgb(249, 249, 249);
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            line-height: 1.08;
+            padding: 2px 0px;
+            white-space: nowrap;
+            position: relative;
+            margin-left: 10px;
+
+            &:before {
+                background-color: rgb(249, 249, 249);
+                border-radius: 0px 0px 4px 4px;
+                bottom: -6px;
+                content: '';
+                left: 0px;
+                height: 2px;
+                opacity: 0;
+                position: absolute;
+                right: 0px;
+                transform-origin: left center;
+                transform: scaleX(0);
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                visibility: hidden;
+                width: auto;
+            }
+        }
+
+
+            &:hover {
+                span: before {
+                    transform: scaleX(1);
+                    visibility: visible;
+                    opacity: 1 !important;
+            }
+        }
+    }
+    @media (max-width: 768px) {
+        display: none;
+    }
 `;
 
 export default Header;
