56df085c641cde5b24287ef5cd332082e8edf5c5
Vineet Parekh <pvineet44@gmail.com>
2021-05-26 17:09:56 +0530

    final build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Detail from './components/Detail';
 import Header from './components/Header';
 import Home from './components/Home';
 import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <Header />
         <Switch>
-          <Route exact path="/">
+          <Route exact path='/'>
             <Login />
           </Route>
-          <Route path="/home">
+          <Route path='/home'>
             <Home />
           </Route>
+          <Route path='/detail/:id'>
+            <Detail />
+          </Route>
         </Switch>
-        </Router>
-
+      </Router>
     </div>
   );
 }
